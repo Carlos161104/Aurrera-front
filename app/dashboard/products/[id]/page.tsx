5cc4a1a0731b502d587c4cb15e853720599ecf97
Carlos161104 <canoja161104@mail.com>
2024-11-01 14:01:16 -0600

    Clase 17

---
+++ app/dashboard/products/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { API_URL } from "@/constants";
+import { Product, Provider } from "@/entities";
+import UpdateProduct from "./_components/UpdateProduct";
+import DeleteProduct from "./_components/DeleteProduct";
+import { AuthHeaders } from "@/helpers/authHeaders";
+export default async function ProductPage({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const responseProduct = await fetch(`${API_URL}/products/${params.id}`, {
+    headers: {
+      ...AuthHeaders(),
+    },
+    next: {
+      tags: [`dashboard:products:${params.id}`],
+    },
+  });
+  const product: Product = await responseProduct.json();
+  const responseProviders = await fetch(`${API_URL}/providers`, {
+    headers: {
+      ...AuthHeaders(),
+    },
+  });
+  const providers: Provider[] = await responseProviders.json();
+  return (
+    <div className="w-full">
+      <div className="bg-yellow-600">
+        <h1 className="w-full font-bold text-white text-center text-2xl py-2">
+          Nombre del producto: {product.productName}
+        </h1>
+        <h2 className="text-xl font-bold text-white text-center">
+          Precio: {product.price}
+        </h2>
+        <h2 className="text-md font-bold text-white text-center py-2">
+          Cant. de Sellos: {product.countSeal}
+        </h2>
+      </div>
+      <UpdateProduct product={product} providers={providers} />
+      <div className="pl-10">
+      <DeleteProduct productId={ product.productId}/>
+
+      </div>
+    </div>
+  );
+}
---
+++ app/dashboard/products/layout.tsx
@@ -0,0 +1,37 @@
+import { API_URL } from "@/constants";
+import { Product } from "@/entities";
+import FilteredCards from "./_components/FilteredCards";
+import { ReactNode } from "react";
+import { AuthHeaders } from "@/helpers/authHeaders";
+const LayoutProducts = async ({children} : {children : ReactNode}) => {
+  const responseProducts = await fetch(`${API_URL}/products`, {
+    headers: {
+      ...AuthHeaders(),
+    },
+    next: {
+      tags: ["dashboard:products"],
+    },
+  });
+  const products: Product[] = await responseProducts.json();
+  const responseProviders = await fetch(`${API_URL}/providers`, {
+    headers: {
+      ...AuthHeaders(),
+    },
+    next: {
+      tags: ["dashboard:providers"]
+    }
+  })
+  const providers = await responseProviders.json()
+  return (
+    <div className="h-[90vh] w-full flex flex-row">
+      <div className="w-3/12">
+        <FilteredCards products={products} providers={providers}/>
+      </div>
+      <div className="w-9/12">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default LayoutProducts;
